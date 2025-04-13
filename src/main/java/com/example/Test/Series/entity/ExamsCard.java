1c72c048b621dd2784f328213d8c1e627fa6e1d1
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2025-04-13 08:58:11 +0530

    Exam card updated

--- src/main/java/com/example/Test/Series/entity/ExamsCard.java
+++ src/main/java/com/example/Test/Series/entity/ExamsCard.java
@@ -1,13 +1,12 @@
 package com.example.Test.Series.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,4 +19,7 @@ public class ExamsCard {
     private String icon;
     private String title;
     private String url;
+
+    @OneToMany(mappedBy = "examsCard", cascade = CascadeType.ALL)
+    private List<ExamTest> examTests;
 }
